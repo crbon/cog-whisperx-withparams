7a59ad68ef788c255fb76ed879beed5a64c01def
wglodell <wglodell@gmail.com>
2023-08-24 00:01:27 +0000

    Add prompt option

--- predict.py
+++ predict.py
@@ -6,6 +6,7 @@ os.environ['TORCH_HOME'] = '/src/torch_models'
 from cog import BasePredictor, Input, Path
 import torch
 import whisperx
+import faster_whisper
 import json
 
 
@@ -22,12 +23,20 @@ class Predictor(BasePredictor):
         audio: Path = Input(description="Audio file"),
         batch_size: int = Input(description="Parallelization of input audio transcription", default=32),
         align_output: bool = Input(description="Use if you need word-level timing and not just batched transcription", default=False),
+        initial_prompt: str = Input(description="Optional text to provide as a prompt for the first window.", default=""),
         only_text: bool = Input(description="Set if you only want to return text; otherwise, segment metadata will be returned as well.", default=False),
         debug: bool = Input(description="Print out memory usage information.", default=False)
     ) -> str:
         """Run a single prediction on the model"""
         with torch.inference_mode():
-            result = self.model.transcribe(str(audio), batch_size=batch_size) 
+            if initial_prompt:
+                print(f"Prompt: {initial_prompt}+")
+                new_asr_options = self.model.options._asdict()
+                new_asr_options["initial_prompt"] = "Your new initial prompt"
+                new_options = faster_whisper.transcribe.TranscriptionOptions(**new_asr_options)
+                print(f"Options: {new_options}+")
+                self.model.options = new_options
+            result = self.model.transcribe(str(audio), batch_size=batch_size)
             # result is dict w/keys ['segments', 'language']
             # segments is a list of dicts,each dict has {'text': <text>, 'start': <start_time_msec>, 'end': <end_time_msec> }
             if align_output:
@@ -36,8 +45,8 @@ class Predictor(BasePredictor):
                 # dict w/keys ['segments', 'word_segments']
                 # aligned_result['word_segments'] = list[dict], each dict contains {'word': <word>, 'start': <start_time_msec>, 'end': <end_time_msec>, 'score': probability}
                 #   it is also sorted
-                # aligned_result['segments'] - same as result segments, but w/a ['words'] segment which contains timing information above. 
-                # return_char_alignments adds in character level alignments. it is: too many. 
+                # aligned_result['segments'] - same as result segments, but w/a ['words'] segment which contains timing information above.
+                # return_char_alignments adds in character level alignments. it is: too many.
             if only_text:
                 return ''.join([val.text for val in result['segments']])
             if debug:
